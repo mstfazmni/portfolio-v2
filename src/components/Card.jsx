8c40c3b998c7d9819c62c26bee3d094d40481853
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-05-12 22:14:44 -0600

    changed to css instead of bootstrap for card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -45,59 +45,64 @@ function Card() {
 
   return (
     <div className="card-wrapper mx-auto">
-      <div className="card bg-dark text-white custom-card">
-        <div className="row no-gutters flex-row-reverse">
-          <div className="col-md-4 position-relative">
-            <img
-              src={currentProject.image}
-              className="card-img"
-              alt={`${currentProject.title} Screenshot`}
-            />
-            <div className="vertical-divider"></div>
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">{currentProject.title}</h5>
-              <p className="card-text">{currentProject.tech}</p>
-              <p className="card-text">{currentProject.description}</p>
-              <a
-                href={currentProject.liveDemo}
-                className="btn btn-success me-2"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Live Demo
-              </a>
-              <a
-                href={currentProject.github}
-                className="btn btn-outline-light"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub Code
-              </a>
+      <div className="custom-card">
+        <div className="card-container">
+          {/* left section of card */}
+          <div className="info-div-left ">
+                  <h5 className="card-title">{currentProject.title}</h5>
+                  <p className="card-text">{currentProject.tech}</p>
+                  <p className="card-text">{currentProject.description}</p>
+                  <div className='card-btns'>
+                      <a
+                        href={currentProject.liveDemo}
+                        className="btn btn-success me-2"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Live Demo
+                      </a>
+                      <a
+                        href={currentProject.github}
+                        className="btn btn-outline-light me-2"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        GitHub Code
+                      </a>
+                </div>
+
+              <div className="vertical-divider"></div>
+            
+                </div>
+                
+          {/* right section of card */}
+            <div className="img-card-right">
+              <img
+                src={currentProject.image}
+                className="card-img"
+                alt={`${currentProject.title} Screenshot`}
+              />
             </div>
           </div>
-          <div className="mt-4 text-center">
-             {/* Floating navigation buttons */}
-              <button
-                className="arrow-button left-arrow"
-                onClick={handlePrev}
-                aria-label="Previous Project"
-              >
-                &#8592;
-              </button>
-              <button
-                className="arrow-button right-arrow"
-                onClick={handleNext}
-                aria-label="Next Project"
-              >
-                &#8594;
-              </button>
-          </div>
         </div>
+        {/* Floating navigation buttons */}
+          <div className='arrow-container'>
+                <button
+                  className="arrow-button left-arrow"
+                  onClick={handlePrev}
+                  aria-label="Previous Project"
+                >
+                  &#8592;
+                </button>
+                <button
+                  className="arrow-button right-arrow"
+                  onClick={handleNext}
+                  aria-label="Next Project"
+                >
+                  &#8594;
+                </button>
+          </div>
       </div>
-    </div>
   );
 }
 
